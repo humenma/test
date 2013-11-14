4066565eec27e85d61adb898cfce96a5b26f426b
sedlakm <sedlak.maros@outlook.com>
2013-11-14 14:23:02 +0100

    koment

--- src/com/mh/appcalculator/MainActivity.java
+++ src/com/mh/appcalculator/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+	}//komentar
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
